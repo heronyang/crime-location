39f2d4a60f778ce7421f73bb42f177611cd5813e
Heron Yang <heron.yang.tw@gmail.com>
2017-01-03 23:12:48 +0800

    add sample code

---
+++ main.R
@@ -0,0 +1,23 @@
+# Main plotting function
+run <- function(font) {
+
+    # Load data
+    dataUrl <- "http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=AB3F3788-E751-4875-A5B8-BF7BD54FD750"
+    data <- read.csv(url(dataUrl))
+
+    # Plot
+    title <- "易發生婦幼被害犯罪地點"
+
+    # Setup margin
+    par(mar = c(4,10,4,4))
+    barplot(table(data$DeptNm), main=title, family=font, las=1, horiz=T)
+
+}
+
+# Plot on screen
+run(font="PingFang TC")
+
+# Save to PDF
+pdf("output.pdf")
+run(font="CNS1")
+dev.off()
